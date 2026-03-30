cf2dc889bf3a3af32c5cdbcc6d8c231267bc5827
j2wicee <joshuaevans2x@gmail.com>
2026-03-30 12:15:33 -0400

    Initial Commit

---
+++ DreamCatcher/DreamCatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamCatcherApp.swift
+//  DreamCatcher
+//
+//  Created by Joshua  Evans  on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct DreamCatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
